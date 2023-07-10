22c2e08c1f3e59f68050612cb58fe556b4d1039d
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-07-10 17:10:31 -0500

    ci: :rocket: add test docker image major

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World con workflow de github actions version major deploy final test!';
+    return 'Hello World con workflow de github actions version major deploy final test asas!';
   }
 }
