59c0318168d1e57b113f7593777a39ebac8a3186
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-07-10 11:43:53 -0500

    BREAKING CHANGE: add new test deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World con workflow de github actions!';
+    return 'Hello World con workflow de github actions version Major!';
   }
 }
